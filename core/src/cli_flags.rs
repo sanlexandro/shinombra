798dd76496eec3c2f97a9effad76f6ea20ee039b
sanlexandro <sanlexandro@proton.me>
2026-07-10 18:10:41 +0300

    feat(service): add service

--- core/src/cli_flags.rs
+++ core/src/cli_flags.rs
@@ -30,7 +30,7 @@ impl CLIFlagsManager {
         let mut args = std::env::args().skip(1);
 
         // Дефолтные значения
-        let mut manifest_path = PathBuf::from("config.toml");
+        let mut manifest_path = PathBuf::from(format!("~/.config/{}/manifest.toml", APP_NAME));
         let mut reset_pipewire_token = false;
         let mut max_log_level = Option::<LogLevel>::None;
         let mut config_path = Option::<PathBuf>::None;
@@ -45,7 +45,7 @@ impl CLIFlagsManager {
                         exit(1);
                     }
                 }
-                "-p" | "--path" => {
+                "--manifest" => {
                     if let Some(next_arg) = args.next() {
                         manifest_path = PathBuf::from(next_arg);
                     } else {
