2dc03f3dd522580bbc1721e43d57fa4e6c19ee4f
sanlexandro <sanlexandro@proton.me>
2026-07-25 23:44:13 +0300

    refactor(screen_capture): add catch_unwind in callback

--- threads/src/screen_capture/handler.rs
+++ threads/src/screen_capture/handler.rs
@@ -1,6 +1,11 @@
 //! Обработчик событий от потока захвата
 
-use std::{ffi::CStr, os::raw::c_void, sync::Arc};
+use std::{
+    ffi::{c_char, CStr},
+    os::raw::c_void,
+    panic::{catch_unwind, AssertUnwindSafe},
+    sync::Arc,
+};
 
 use common::core::{controller::CoreController, event_handlers::EventHandler};
 use ffi::bindings::CaptureEvent;
@@ -45,31 +50,38 @@ impl EventHandler<CaptureEventWrapper> for CaptureHandler {
     }
 }
 
-/// Функция, которую будет дёргать pipewire из C
 pub(crate) extern "C" fn callback(
     user_data: *mut c_void,
     event: CaptureEvent,
-    message: *const std::os::raw::c_char,
+    message: *const c_char,
 ) {
-    if user_data.is_null() {
-        return;
+    // catch_unwind перехватывает паники внутри замыкания
+    let result = catch_unwind(AssertUnwindSafe(|| {
+        if user_data.is_null() {
+            return;
+        }
+
+        let arc_ptr = user_data as *const CoreController;
+        let controller = std::mem::ManuallyDrop::new(unsafe { Arc::from_raw(arc_ptr) });
+
+        let string_message = if message.is_null() {
+            String::new()
+        } else {
+            unsafe { CStr::from_ptr(message) }
+                .to_string_lossy()
+                .into_owned()
+        };
+
+        let wrapper = CaptureEventWrapper {
+            event,
+            message: string_message,
+        };
+
+        CaptureHandler::handle(wrapper, &controller);
+    }));
+
+    if let Err(err) = result {
+        // Логируем или обрабатываем панику, не давая ей уйти в C
+        error!("Panic caught in Pipewire callback: {:?}", err);
     }
-
-    let arc_ptr = user_data as *const CoreController;
-    let controller = std::mem::ManuallyDrop::new(unsafe { Arc::from_raw(arc_ptr) });
-
-    let string_message = if message.is_null() {
-        String::new()
-    } else {
-        unsafe { CStr::from_ptr(message) }
-            .to_string_lossy()
-            .into_owned()
-    };
-
-    let wrapper = CaptureEventWrapper {
-        event: event,
-        message: string_message,
-    };
-
-    CaptureHandler::handle(wrapper, &controller);
 }
